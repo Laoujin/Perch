d798a261f9649893779c9d811b59a2338a07c21c
Wouter Van Schandevijl <woutervs@hotmail.com>
2026-02-16 23:00:24 +0100

    Remove old Avalonia wizard ViewModels

--- src/Perch.Desktop/ViewModels/Wizard/WizardStepViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Perch.Desktop.ViewModels.Wizard;
-
-public abstract class WizardStepViewModel : ViewModelBase
-{
-    public abstract string Title { get; }
-    public abstract int StepNumber { get; }
-    public virtual bool CanSkip => true;
-    public virtual bool CanGoBack => StepNumber > 1;
-}
